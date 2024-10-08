a835bdf11d0fc61cbcd83b0cf335b30106ac82ab
Diego Diaz De La Concha <diegodiazdelaconcha93@gmail.com>
2024-10-07 18:26:55 -0600

    componente Card y estilos finalizados

--- main.js
+++ main.js
@@ -25,6 +25,7 @@ function DATA(event) {
     const title = document.querySelector('.card-title');
     const description = document.querySelector('.card-text1');
     const recommendations = document.querySelector('.card-text2');
+    const img = document.querySelector('.img-fluid');
 
     const CALC = weight / (height / 100) ** 2;
 
@@ -32,38 +33,39 @@ function DATA(event) {
 
     if (CALC < 18.5){
         title.innerText = "Bajo Peso";
-        description.innerText = "Tu peso es inferior al normal, te recomendamos acudir con un especialista para que te ayude a mejorar tu alimentación.";
-        recommendations.innerText = "Te recomendamos consumir alimentos ricos en proteínas y carbohidratos.";
+        description.innerText = "Peso insuficiente para la altura, posible riesgo de desnutrición";
+        recommendations.innerText = "Aumentar calorías con alimentos nutritivos y hacer ejercicios de fuerza.";
+        img.src = "https://media.istockphoto.com"
     }
     else if(CALC >= 18.5 && CALC <= 24.9){
 
         title.innerText = "Normal";
-        description.innerText = "Tu peso es normal, te recomendamos seguir con una alimentación balanceada y realizar ejercicio.";
-        recommendations.innerText = "";
+        description.innerText = "Peso adecuado para la altura, asociado con buena salud general.";
+        recommendations.innerText = "Mantener una dieta balanceada y hacer ejercicio regularmente.";
     }
     else if(CALC >= 25 && CALC <= 29.9){
 
         title.innerText = "Sobrepeso";
-        description.innerText = "Tu peso es superior al normal, te recomendamos acudir con un especialista para que te ayude a mejorar tu alimentación.";
-        recommendations.innerText = "";
+        description.innerText = "Exceso de peso que aumenta el riesgo de enfermedades";
+        recommendations.innerText = "Reducir alimentos ultraprocesados, mejorar la dieta y aumentar la actividad física";
     }
     else if(CALC >= 30 && CALC <= 34.9){
 
         title.innerText = "Obesidad Grado 1";
-        description.innerText = "";
-        recommendations.innerText = "";
+        description.innerText = "Aumento significativo del riesgo de enfermedades crónicas";
+        recommendations.innerText = "Adoptar una dieta baja en calorías, aumentar el ejercicio y consultar a un especialistas";
     }
     else if(CALC >= 35 && CALC <= 39.9){
 
         title.innerText = "Obesidad Grado 2";
-        description.innerText = "";
-        recommendations.innerText = "";
+        description.innerText = "Obesidad severa con alto riesgo de problemas de salud graves.";
+        recommendations.innerText = "Seguir un plan supervisado por especialistas en nutrición y ejercicio.";
     }
     else if(CALC >= 40){
 
         title.innerText = "Obesidad Grado 3";
-        description.innerText = "";
-        recommendations.innerText = "";
+        description.innerText = "Obesidad mórbida con riesgo extremo de complicaciones de salud.";
+        recommendations.innerText = "Buscar tratamiento médico especializado, considerar opciones como cirugía bariátrica y apoyo psicológico";
     }
     else if(isNaN(CALC)){
         throw new Error("Error en el calculo");
